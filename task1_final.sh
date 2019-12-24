3908437b69fd181faac9a1e67eac45534dabc46e
alina143 <52449274+alina143@users.noreply.github.com>
2019-12-25 03:34:03 +0700

    Update task1_final.sh

--- task1_final.sh
+++ task1_final.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$PATH" | tr ':' '\n' | while read name; do ls -ld "$name"/* ; done | awk '{print $5 " " $9}'| while read len name; do if [ -f "$name" ]; then echo $len "$name"; fi; done | sort -nr | head -n 1 | ls -lh "$name"
+echo "$PATH" | tr ':' '\n' | while read name; do ls -ld "$name"/* ; done | awk '{print $5 " " $9}'| while read len name; do if [ -f "$name" ]; then echo $len "$name"; fi; done | sort -nr | head -n 1 
